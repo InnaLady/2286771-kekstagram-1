cf01f6049ec2e60a5f5da30dd778501cc160c484
Inna Ladyzhenskaia <pinguinil2000@gmail.com>
2024-05-30 01:15:31 +0300

    проходит проверку через lint

--- js/util.js
+++ js/util.js
@@ -5,8 +5,6 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min); // Максимум и минимум включаются
 };
 
-
-
 function getRandomIdfromRangeGenerator(min, max) {
   const previousValues = [];
   return function () {
